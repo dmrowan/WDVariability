a24f89708138b05d57d5bf5ff11948b898844ebc
Dominick <dmrowan@haverford.edu>
2018-08-08 00:01:27 -0400

    Latex command generation

--- WDGenerate.py
+++ WDGenerate.py
@@ -3,17 +3,72 @@ from __future__ import print_function, division
 import os
 import argparse
 import WDAssign
-import WDPlot_eclipse
 import WDPlot_pulsator
 import WDColorMag
 import WDsigmamag
-import WDlatexcommands
 import WDmaghist
+import WD_EB
+from WD_EB import WDEB
 #Dom Rowan REU 2018
 
 desc="""
 WDGenerate: Generate all plots, tables, for paper
 """
+
+def latexcommands(printcommands=False):
+    if os.path.isfile("WDcommands.tex"):
+        print("Overwriting current WDcommands.tex file")
+        subprocess.run(['rm', 'WDcommands.tex'])
+
+    #Survey size
+    df_catalog = pd.read_csv("Catalogs/MainCatalog_reduced_simbad_asassn.csv")
+    n_survey = "{:,}".format(len(list(set(list(df_catalog['MainID'])))))
+    survey_command = "\\newcommand{\\surveysize}{"+n_survey+" }"
+
+    #Initial catalog size
+    df_ic = pd.read_csv("Catalogs/Allsources_allexpt.csv")
+    MainID_list = []
+    for i in range(len(df_ic)):
+        if str(df_ic['MWDDDesignation'][i]) != 'nan':
+            MainID_list.append(df_ic['MWDDDesignation'][i])
+        elif str(df_ic['ATLASDesignation'][i]) != 'nan':
+            MainID_list.append(df_ic['ATLASDesignation'][i])
+        elif str(df_ic['SDSSDesignation'][i]) != 'nan':
+            MainID_list.append(df_ic['SDSSDesignation'][i])
+        else:
+            MainID_list.append(df_ic['GaiaDesignation'][i])
+
+    n_initial = "{:,}".format(len(list(set(list(MainID_list)))))
+    init_survey_command = "\\newcommand{\\initsize}{"+n_initial+" }"
+
+    #Interesting source numbers
+    n_eclipses = "{:,}".format(len(os.listdir("InterestingSources/Eclipse")))
+    n_new_pulsators = "{:,}".format(len(os.listdir(
+        "InterestingSources/Pulsator")))
+    n_known_pulsators = "{:,}".format(len(os.listdir(
+        "InterestingSources/KnownPulsator")))
+
+    eclipse_command = "\\newcommand{\\neclipse}{"+n_eclipses+" }"
+    new_pulsators_command = ("\\newcommand{\\nnewpulsator}{"
+                             +n_new_pulsators+" }")
+    known_pulsators_command = ("\\newcommand{\\nknownpulsators}{"
+                               +n_known_pulsators+" }")
+
+    #Write file
+    with open('WDcommands.tex', 'w') as f:
+        f.write(survey_command + "\n")
+        f.write(init_survey_command + "\n")
+        f.write(eclipse_command + "\n")
+        f.write(new_pulsators_command + "\n")
+        f.write(known_pulsators_command + "\n")
+
+    if printcommands:
+        print(survey_command)
+        print(init_survey_command)
+        print(eclipse_command)
+        print(new_pulsators_command)
+        print(known_pulsators_command)
+
 
 def main(eclipsegenerate, pulsatorgenerate):
     #Path assertions 
@@ -25,9 +80,10 @@ def main(eclipsegenerate, pulsatorgenerate):
     os.chdir('InterestingSources')
     WDAssign.main(False)
     WDAssign.latextable()
+    WDAssign.latex_known()
 
-    #Run WDPlot_eclipse.py
-    WDPlot_eclipse.main(eclipsegenerate)
+    #Run WD_EB
+    WD_EB.main(iterations=1000)
 
     #Run WDPlot_pulsator.py
     WDPlot_pulsator.main(pulsatorgenerate)
@@ -37,7 +93,8 @@ def main(eclipsegenerate, pulsatorgenerate):
     
     #Run WDColorMag.py
     os.chdir('../GalexData_run6')
-    WDColorMag.main(False, False, False,None, True, False)
+    WDColorMag.main(False, False, False,None, True, False, True)
+    WDColorMag.main(False, False, False, None, True, False, False)
 
     #Run WDsigmamag.py
     WDsigmamag.percentile(True)
@@ -45,7 +102,7 @@ def main(eclipsegenerate, pulsatorgenerate):
     #Run WDlatexcommands.py
     os.chdir('../')
     assert(os.getcwd() == '/home/dmrowan/WhiteDwarfs')
-    WDlatexcommands.main(False)
+    latexcommands
     
 
 if __name__ == '__main__':
