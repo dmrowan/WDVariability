e891800a12c87a1a6d55514bee98f8d7f8be8355
Dominick <dmrowan@haverford.edu>
2018-08-11 14:31:53 -0400

    Test cases

--- WD_Recovery.py
+++ WD_Recovery.py
@@ -2,9 +2,7 @@
 from __future__ import print_function, division, absolute_import
 import argparse
 from astropy.stats import LombScargle
-import collections
 from gPhoton import gphoton_utils
-import heapq
 import matplotlib.pyplot as plt
 import multiprocessing as mp
 import numpy as np
@@ -12,11 +10,17 @@ import os
 import pandas as pd
 import _pickle as pickle
 from progressbar import ProgressBar
-import WDranker_2
-import WDutils
 import random
 import time
+import WDranker_2
+import WDutils
 
+desc="""
+WD_Recovery: Procedure for injecting and recovering synthetic optical 
+data from WD-1145.
+"""
+
+#Define a class for a galex visit df
 class Visit:
     def __init__(self, df):
         self.df = df
@@ -25,7 +29,13 @@ class Visit:
 
     #Simple check if we have enough usable data
     def good_df(self):
-        if len(self.df['t1']) == 0:
+        #Create a second data frame with fully reduced data
+        #Use this as a way to check flag points
+        df2 = WDutils.df_fullreduce(self.df)
+        if len(df2['t1']) <= 10:
+            return False
+
+        elif len(self.df['t1']) == 0:
             return False
         else:
             exposuretup = WDranker_2.find_cEXP(self.df)
@@ -60,11 +70,12 @@ class Visit:
         df_optical = selectOptical(opticalLC, 
                                    exposure=self.exposure/60, plot=plot)
 
+        #linear interpolation to match times
         optical_flux = np.interp(t_mean, df_optical['time'], 
                                  df_optical['flux'])
 
+        #Scale by multiplicative factor
         optical_flux = optical_flux * mf
-
         flux_injected = [ flux[i] * optical_flux[i] 
                               for i in range(len(flux)) ]
 
@@ -77,6 +88,7 @@ class Visit:
                                                coloredtup.bluepoints]))
         self.df = self.df.drop(index=droppoints)
         self.df = self.df.reset_index(drop=True)
+
         t_mean = self.df['t_mean']
         self.flux_injected = self.df['flux_bgsub']
         flux_err = self.df['flux_bgsub_err']
@@ -99,6 +111,7 @@ class Visit:
         freq_detrad, amp_detrad = ls_detrad.autopower(nyquist_factor=1)
         pgram_tup = WDranker_2.find_cPGRAM(ls, amp_detrad, 
                                            exposure=self.exposure)
+        #Return 0,1 rseult of recovery
         c_periodogram = pgram_tup.c
         ditherperiod_exists = pgram_tup.ditherperiod_exists
         if (not ditherperiod_exists) and (c_periodogram > 0):
@@ -140,6 +153,7 @@ def genMagLists(plot=False):
     #Iterate through NUV files in cwd
     for filename in pbar(os.listdir(os.getcwd())):
         if filename.endswith('.csv') and 'NUV' in filename:
+            #Do a full data reduction
             alldata = pd.read_csv(filename)
             alldata = WDutils.df_fullreduce(alldata)
             alldata = WDutils.tmean_correction(alldata)
@@ -172,7 +186,7 @@ def genMagLists(plot=False):
 
         fig.savefig("/home/dmrowan/WhiteDwarfs/NUVmaghist.pdf")
 
-    #Save pickle
+    #Save as pickle
     mag_array = np.array(mag_list)
     path_array = np.array(path_list)
     tup = (mag_array,path_array)
@@ -180,6 +194,7 @@ def genMagLists(plot=False):
         pickle.dump(tup, handle)
 
 
+#Choose a source and visit
 def selectLC(binvalue, mag_array, path_array):
     #First need to find indicies for bin
     binupper = binvalue + .1
@@ -196,6 +211,7 @@ def selectLC(binvalue, mag_array, path_array):
                'flux_bgsub', 'flux_bgsub_err',
                'detrad', 'flags', 'exptime']
     alldata = pd.read_csv(filename, usecols=usecols)
+    #Data reduction and time correction
     alldata = WDutils.df_reduce(alldata)
     alldata = WDutils.tmean_correction(alldata)
     #Split into visits 
@@ -207,15 +223,16 @@ def selectLC(binvalue, mag_array, path_array):
         visit = Visit(df)
         if visit.good_df() == True: 
             if visit.existingperiods() == False:
-                print(df)
+                print
                 visit_list.append(visit)
 
-    print(len(visit_list))
+    #If there were no good visits, pick a new source
     if len(visit_list) == 0:
+        print(filename, "no sources in visit list")
         visit, source_mag = selectLC(binvalue, mag_array, path_array)
         return visit, source_mag
+    #Select random visit
     else:
-        #Select random visit
         visit = random.choice(visit_list)
         return visit, source_mag
 
@@ -224,6 +241,7 @@ def selectLC(binvalue, mag_array, path_array):
 def selectOptical(opticalLC, plot=False, exposure=30):
     exphalf = exposure/2
     maxtime = max(opticalLC['time'])
+    #Find idx range corresponding to exposure
     idx_low = np.where(opticalLC['time'] < exphalf)[0][-1]
     idx_high = np.where(opticalLC['time'] > maxtime-exphalf)[0][0]
     idx_center = np.arange(idx_low, idx_high, 1)
@@ -232,6 +250,7 @@ def selectOptical(opticalLC, plot=False, exposure=30):
     df_optical = opticalLC[(opticalLC['time'] > time_center-exphalf) 
                           &(opticalLC['time'] < time_center+exphalf)]
     df_optical = df_optical.reset_index(drop=True)
+    #Reset time to start from zero
     tmin = df_optical['time'][0]
     for i in range(len(df_optical['time'])):
         df_optical.loc[i, 'time'] = df_optical['time'][i] - tmin
@@ -242,74 +261,141 @@ def selectOptical(opticalLC, plot=False, exposure=30):
         plt.show()
     return df_optical
 
-def main(mag, mag_array, path_array, opticalLC):
-
+#Full injection procedure for single source
+def main(mag, mag_array, path_array, opticalLC, fname=None):
+    #Select a visit
     visit, source_mag = selectLC(mag, mag_array, path_array)
+
+    #Select a multiplicative factor
     mf = random.choice(np.arange(0, 2, .1))
     mf = round(mf, 1) #np.arange has some odd behaviors
     visit.inject(opticalLC, mf)
     result = visit.assessrecovery()
     tup = (mag, source_mag, mf, result)
-    fname = str(mag)+"_bin.txt"
+
+    #Generate output string
     outputstr = (str(source_mag)+","
                 +str(mf)+","
                 +str(result))
-    os.system("echo {0} >> {1}".format(outputstr, fname))
+
+    #Append to outputfile
+    if fname is not None:
+        os.system("echo {0} >> {1}".format(outputstr, fname))
+
     print(outputstr)
     return tup
 
-"""
-def wrapper(mag_array, path_array, opticalLC, iterations=1):
-    bins = np.arange(min(mag_array), max(mag_array)+.1, .1)
-    pool = mp.Pool(processes = mp.cpu_count()+2)
+def wrapper(mag_array, path_array, opticalLC, 
+            iterations, ml, mu, bs, p, fname):
+    #Set up magnitude bins
+    minbin=ml
+    maxbin=mu
+    bins = np.arange(minbin, maxbin+bs, bs)
+    bins = np.array([ round(b, 1) for b in bins ])
+
+    #Create mp pool and iterate
+    pool = mp.Pool(processes = p)
     jobs=[]
+    print(iterations)
     for i in range(iterations):
         for mag in bins:
             job = pool.apply_async(main, args=(mag, mag_array, 
-                                               path_array, opticalLC))
+                                               path_array, opticalLC, 
+                                               fname,))
             jobs.append(job)
     for job in jobs:
-        result_tup = job.get()
-        magbin = result_tup[0]
-        fname = str(magbin)+"_bin.txt"
-        outputstr = (str(result_tup[1])+","
-                    +str(result_tup[2])+","
-                    +str(result_tup[3]))
-        os.system("echo {0} >> {1}".format(outputstr, fname))
-"""
-def wrapper(mag_array, path_array, opticalLC, iterations=1):
-    minbin = 16
-    maxbin = 21
-    #numpy arange doesn't always play nice
-    bins = np.arange(minbin, maxbin+.1, .1)
-    bins = np.array([ round(b, 1) for b in bins ])
-    for i in range(iterations):
-        for mag in bins:
-            main(mag, mag_array, path_array, opticalLC)
+        job.get()
 
+#Iterate through all sources and all visits 
+def testfunction(path_array):
+    usecols = ['t0', 't1', 't_mean',
+               'mag_bgsub',
+               'cps_bgsub', 'cps_bgsub_err', 'counts',
+               'flux_bgsub', 'flux_bgsub_err',
+               'detrad', 'flags', 'exptime']
+    for filename in path_array:
+        assert(os.path.isfile(filename))
+        alldata = pd.read_csv(filename, usecols=usecols)
+        #Data reduction and time correction
+        alldata = WDutils.df_reduce(alldata)
+        alldata = WDutils.tmean_correction(alldata)
+        #Split into visits 
+        data = WDutils.dfsplit(alldata, 100)
+        source_mag = round(np.nanmedian(alldata['mag_bgsub']),5)
+        visit_list = []
+        for df in data:
+            visit = Visit(df)
+            if visit.good_df() == True: 
+                if visit.existingperiods() == False:
+                    print
+                    visit_list.append(visit)
 
+        #If there were no good visits, pick a new source
+        if len(visit_list) == 0:
+            print(filename, "no sources in visit list")
+            continue
+        for visit in visit_list:
+            mf = random.choice(np.arange(0, 2, .1))
+            mf = round(mf, 1) #np.arange has some odd behaviors
+            try:
+                visit.inject(opticalLC, mf)
+                result = visit.assessrecovery()
+            except:
+                result = "Error with source "+filename
+
+            #Generate output string
+            outputstr = (str(source_mag)+","
+                        +str(mf)+","
+                        +str(result))
+
+            print(outputstr)
+            fname = 'testoutput.txt'
+            os.system("echo {0} >> {1}".format(outputstr, fname))
+
+    
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("--iter", help="Number of iterations to run", 
+                        type=int, default=100)
+    parser.add_argument("--ml", help="Lower mag range", 
+                        type=float, default=16)
+    parser.add_argument("--mu", help="Upper mag range", 
+                        type=float, default=21)
+    parser.add_argument("--bs", help="Magnitude bin size", 
+                        type=float, default=.1)
+    parser.add_argument("--p", help="Number of processes to spawn", 
+                        type=int, default=4)
+    parser.add_argument("--output", help="Output filename to save results",
+                        type=str, default=None)
+    args=parser.parse_args()
+
+    #Argument assertions
+    assert(args.iter > 0)
+    assert( (args.ml > 10) and (args.ml < 30) and (args.ml < args.mu) )
+    assert( (args.mu > 10) and (args.mu < 30) )
+    assert( (args.bs >= .1) and (args.bs <=2 ) )
+    assert(args.p >= 1)
+
+    #Pickle loads
     with open('1145LC.pickle', 'rb') as p:
         opticalLC = pickle.load(p)
     with open('MagList.pickle', 'rb') as p2:
         MagList = pickle.load(p2)
     
-    
     mag_array = MagList[0]
     path_array = MagList[1]
 
-    """
-    visit = selectLC(19, np.array([19]), 
-                     np.array(
-                         ['GalexData_run6/SDSS-J222816.29+134714.4-NUV.csv']))
-    end = time.time()
-    visit.inject(opticalLC, 1, plot=True)
-    result = visit.assessrecovery()
-    """
-    #result = main(19, mag_array, path_array, opticalLC)
-    #print(result)
-    #wrapper(mag_array, path_array, opticalLC)
+    #wrapper(mag_array, path_array, opticalLC, args.iter, args.ml,
+    #        args.mu, args.bs, args.p, args.output)
 
-    visit, source_mag = selectLC(1, np.array([1]), np.array(['WD-1238+127-NUV.csv']))
-    visit.inject(opticalLC, 1, plot=True)
+    testfunction(path_array)
+    
+
+
+    #for i in range(50):
+    #    visit, source_mag = selectLC(1, np.array([1]), np.array(['WD-0102+004-NUV.csv']))
+    #    visit.inject(opticalLC, 1, plot=False)
+    #    result = visit.assessrecovery()
+    #    print(result)
 
