f6bf275be3922fa7099d67915a7b35bafbd182a7
Dominick <dmrowan@haverford.edu>
2018-08-19 13:41:31 -0400

    formatting changes

--- WDviewer.py
+++ WDviewer.py
@@ -6,13 +6,18 @@ import pandas as pd
 import subprocess
 #Dom Rowan REU 2018
 
-desc="""Three functional options: view/comment on functions in range --f to --l (default), select specific idx --i to view/comment, or move index --move to interesting sources directory under specific headers"""
+desc="""
+Three functional options: 
+    (1) view/comment on functions in range  
+    (2) select specific idx --i to view/comment 
+    (3) Move interesting sources directory under specific headers
+"""
 
 #View and comment on sources in range f, l+1
 def main(f,l):
     assert(os.path.isfile("Output/AllData.csv"))
     df_rank = pd.read_csv("Output/AllData.csv")
-    #Use the indicies on the csv, not the index column that exists within parsed df
+    #Use the indicies on the csv 
     f = f-2
     l = l-2
     for i in range(f,l+1):
@@ -39,9 +44,11 @@ def main(f,l):
                 #Account for special characters
                 if '[' in source:
                    source = source.replace('[', "\[").replace(']','\]')
-                   filename = "PDFs/{0}_{1}_combined.pdf".format(source, band)
+                   filename = "PDFs/{0}_{1}_combined.pdf".format(source, 
+                                                                 band)
                 else:
-                    filename = "PDFs/{0}_{1}_combined.pdf".format(source, band)
+                    filename = "PDFs/{0}_{1}_combined.pdf".format(source, 
+                                                                  band)
                 #Open PDF
                 subprocess.run(['xdg-open', filename])
 
@@ -49,7 +56,10 @@ def main(f,l):
         if str(df_rank['Comment'][i]) != 'nan':
             print("Current comment: ", df_rank['Comment'][i])
 
-        comment = input(df_rank['SourceName'][i]+" -- Enter comment to save into csv. Hit enter for no comment or to keep existing comment --- ")
+        comment = input(df_rank['SourceName'][i]+
+                        " -- Enter comment to save into csv." +
+                        "Hit enter for no comment or to keep" +
+                        "existing comment --- ")
         if comment == "break":
             break
         elif len(comment) != 0:
@@ -78,12 +88,16 @@ def selectidx(n, comment=True):
     if str(df_rank['Comment'][i]) != 'nan':
         print("Current comment: ", df_rank['Comment'][i])
     if comment:
-        comment = input("Enter comment to save into csv. Hit enter for no comment or to keep existing comment --- ")
+        comment = input("Enter comment to save into csv. " +
+                        "Hit enter for no comment or to keep " +
+                        "existing comment --- ")
         if len(comment) != 0:
             df_rank.loc[i, 'Comment'] = comment
             
         df_rank.to_csv("Output/AllData.csv", index=False)
 
+def name(source, comment=True):
+    assert(os.path.isfile("Output/Alldata.csv"))
 #Add source to InterestingSources 
 def move(i):
     assert(os.path.isfile("Output/AllData.csv"))
@@ -102,8 +116,13 @@ def move(i):
         pdffilename = "PDFs/{0}_{1}_combined.pdf".format(source, band)
         csvfilename = "{0}-{1}.csv".format(source, band)
     #User input - select object type for categorization
-    objecttype = input("Enter category to move to: Pulsator, KnownPulsator, PossiblePulsator, or Eclipse --- ")
-    valid_object_types = ["Pulsator", "KnownPulsator","PossiblePulsator", "Eclipse"]
+    objecttype = input("Enter category to move to: " +
+                       "Pulsator, KnownPulsator, PossiblePulsator, " +
+                       "or Eclipse --- ")
+    valid_object_types = ["Pulsator", 
+                          "KnownPulsator",
+                          "PossiblePulsator", 
+                          "Eclipse"]
     if not (objecttype in valid_object_types):
         print("Invalid object type")
         return
@@ -111,29 +130,48 @@ def move(i):
         #Check if directory exists for source ID (FUV and NUV share directory)
         if os.path.isdir("../InterestingSources/"+objecttype+"/"+source):
             print("Directory already exists")
-            if not os.path.isdir("../InterestingSources/"+objecttype+"/"+source+'/PDFs'):
-                subprocess.run(['mkdir', '../InterestingSources/'+objecttype+'/'+source+'/PDFs'])
+            if not os.path.isdir("../InterestingSources/"+
+                                 objecttype+"/"+source+'/PDFs'):
+                subprocess.run(['mkdir', '../InterestingSources/'+
+                                objecttype+'/'+source+'/PDFs'])
         else:
             print("Making directory for source")
-            subprocess.run(['mkdir', '../InterestingSources/'+objecttype+'/'+source])
-            subprocess.run(['mkdir', '../InterestingSources/'+objecttype+'/'+source+'/PDFs'])
+            subprocess.run(['mkdir', '../InterestingSources/'+
+                            objecttype+'/'+source])
+            subprocess.run(['mkdir', '../InterestingSources/'+
+                            objecttype+'/'+source+'/PDFs'])
 
         #Move pdf file and csvfile
-        subprocess.run(['cp', pdffilename, '../InterestingSources/'+objecttype+"/"+source+"/PDFs/"])
-        subprocess.run(['cp', csvfilename, '../InterestingSources/'+objecttype+"/"+source+"/"])
+        subprocess.run(['cp', pdffilename, '../InterestingSources/'+
+                        objecttype+"/"+source+"/PDFs/"])
+        subprocess.run(['cp', csvfilename, '../InterestingSources/'+
+                        objecttype+"/"+source+"/"])
         
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument("--i", help="Select row of source to view, opening pdf in window", default=None, type=int)
-    parser.add_argument("--f", help="If using fromtop, identify first index", default=2, type=int)
-    parser.add_argument("--l", help="If using fromtop, identify last index", default=12, type=int)
-    parser.add_argument("--move", help="Move file in index to Interesting sources", default=None, type=int)
+    parser.add_argument("--i", 
+            help="Select row of source to view, opening pdf in window", 
+            default=None, type=int)
+    parser.add_argument("--f", 
+            help="If using fromtop, identify first index", 
+            default=2, type=int)
+    parser.add_argument("--l", 
+            help="If using fromtop, identify last index", 
+            default=12, type=int)
+    parser.add_argument("--move", 
+            help="Move file in index to Interesting sources", 
+            default=None, type=int)
+    parser.add_argument("--name", 
+            help="Select source to view by name, opening pdf in window", 
+            default=None, type=str)
     args= parser.parse_args()
 
     if args.move is not None:
         move(args.move)
+    elif args.name is not None:
+        name(args.name)
     elif args.i is not None:
         selectidx(args.i)
     else:
